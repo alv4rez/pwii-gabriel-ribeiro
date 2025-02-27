2dab0e0bf486fa5d3ee7f2f339327d44511e61f3
Gabriel Alvarez <alvarezgbr01@gmail.com>
2025-02-27 07:44:05 -0300

    Aula básica de PHP

---
+++ Aula02/concaternar.php
@@ -0,0 +1,8 @@
+<?php ECHO "Hello, World!<br>";
+    echo "Hello, World!<br>";
+    EcHo "Hello, World!<br>";
+    $nome = "Gabriel";
+    $sobrenome = "Alvarez";
+    $numero = 4; 
+    echo "${nome} <h1> ${sobrenome} <h1>";
+    echo $numero . "<p> ". $nome . " " . $sobrenome . "</p>"; ?>
