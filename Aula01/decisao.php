6be3ed352086505137f9ea16761bbfe50fb49d72
Gabriel Alvarez <alvarezgbr01@gmail.com>
2025-03-12 10:35:53 -0300

    Aula sobre variaveis e decisao

---
+++ Aula01/decisao.php
@@ -0,0 +1,28 @@
+<?php
+    $n = 6;
+    
+    if ($n >= 4) {
+        echo $n, " é maior ou igual a 4 + uma aleração quase plausível";
+    }
+
+    $n1 = 3;
+    $n2 = 4;
+
+    $result = ($n1%$n2)%2;
+
+    if ($result == 1) {
+        echo "<br>", $result, " quociente impar de novo";
+    } else{
+        echo "<br>", $result, " quociente de par.";
+    }
+
+    $n3 = 10;
+    $n4 = null;
+
+    if ($n3) {
+        echo "a variavel tem valor!";
+    } else{
+        echo "a variavel não tem valor";
+    }
+    
+?>
