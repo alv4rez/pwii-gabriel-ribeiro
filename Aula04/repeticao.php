4a771888bdf5346a646ec39e4d854bc1cdba1e81
Gabriel Alvarez <alvarezgbr01@gmail.com>
2025-03-24 07:04:09 -0300

    Aula sobre laço de repetição

---
+++ Aula04/repeticao.php
@@ -0,0 +1,28 @@
+<?php
+
+$i =1;
+
+while ($i < 6){
+    echo $i;
+    $i++;
+    echo "<br>";
+}
+
+echo "____ <br>";
+
+$i = 7;
+
+
+do{
+    echo $i;
+    $i++;
+    echo "<br>";
+} while ($i < 6 );
+
+echo "____ <br>";
+
+for($x = 0; $x <= 10; $x++){
+    echo "O número é: $x <br>";
+}
+
+?>
